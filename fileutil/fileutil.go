259e74e2b505923620c7ce15585416e4fa07995a
Mikko Forsman <mikko.o.forsman@gmail.com>
2017-06-07 22:13:54 +0300

    Test progress

--- fileutil/fileutil.go
+++ fileutil/fileutil.go
@@ -16,17 +16,25 @@ func GetFileContents(path, fileName string) string {
 	return string(data)
 }
 
-//AddLineToFile appends a file named fileName at path with line
-func AddLineToFile(line, path, fileName string) {
+//AddToFile appends a file named fileName at path with line
+func AddToFile(text, path, fileName string) {
 	fullPath := CreateFullPath(path, fileName)
 	//Make sure that the file exists
-	if _, err := os.Stat(fullPath); os.IsNotExist(err) {
+	if !fileExists(path, fileName) {
 		createFile(path, fileName)
 	}
 	file, err := os.OpenFile(fullPath, os.O_APPEND, 0666)
 	checkError(err)
-	_, err = file.WriteString(line + "\n")
+	_, err = file.WriteString(text)
 	checkError(err)
+}
+
+//EmptyFile removes the contents of a file
+func EmptyFile(path, fileName string) {
+	if !fileExists(path, fileName) {
+		panic("Can't empty non-existent file!")
+	}
+	ioutil.WriteFile(CreateFullPath(path, fileName), []byte(""), 0644)
 }
 
 //CreateFullPath combines the path and filename to an actual path
@@ -49,6 +57,13 @@ func createFile(path, fileName string) {
 	checkError(err)
 	_, err = os.Create(fileName)
 	checkError(err)
+}
+
+func fileExists(path, fileName string) bool {
+	if _, err := os.Stat(CreateFullPath(path, fileName)); os.IsNotExist(err) {
+		return false
+	}
+	return true
 }
 
 func checkError(err error) {
--- fileutil/fileutil_test.go
+++ fileutil/fileutil_test.go
@@ -22,7 +22,29 @@ func TestCreateFullPath(t *testing.T) {
 	}
 }
 
-func TestAddLineToFile(t *testing.T) {
-	//TODO: test
-	t.Error("Test not implemented")
+func TestGetFileContents(t *testing.T) {
+	content := "test_read_string"
+	result := GetFileContents("fileutil_test", "test_read.txt")
+	if result != content {
+		t.Error("Expected " + content + ", got " + result)
+	}
+}
+
+func TestAddToFile(t *testing.T) {
+	//File should be currently empty
+	testString := "test_write_string"
+	path := "fileutil_test"
+	fileName := "test_add.txt"
+	EmptyFile(path, fileName)
+	AddToFile(testString, path, fileName)
+	content := GetFileContents(path, fileName)
+	println(content)
+	if content != testString {
+		t.Error("File contents should be " + testString + ", was " + content)
+	}
+	AddToFile(testString, path, fileName)
+	content = GetFileContents(path, fileName)
+	if content != (testString + testString) {
+		t.Error("File contents should be " + testString + testString + ", was " + content)
+	}
 }
